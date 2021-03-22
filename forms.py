ebb9ca8dec6d67f935bc20b53c004013692e349e
Gael Gil <gaelgilbello27@gmail.com>
2021-03-22 14:03:04 -0700

    fixed lots of frontedn issues

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class BookForm(FlaskForm):
-    book = StringField('Book', validators=[DataRequired(), Length(min=60)])
+    book = StringField('Text', validators=[DataRequired(), Length(min=100)])
     submit = SubmitField('Submit')
 
 class Make_Sentece_Form(FlaskForm):
