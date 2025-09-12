3e9be64bd9ea6e4c2e7b6285d5a36fe9be317025
Gael <gaelgilbello27@gmail.com>
2025-09-12 16:10:11 -0700

    creating the generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 export default function App() {
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
   const [generatedContent, setGeneratedContent] = useState("");
+
+  useEffect(() => {}, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     e.preventDefault();
     setContent(e.target.value);
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
     try {
       console.log(loading);
-
+      setGeneratedContent(content);
       alert(content);
     } catch (error) {
       console.log(error);
@@ -25,7 +27,8 @@ export default function App() {
 
   return (
     <div className="max-w-4xl mx-auto p-8">
-      <h4 className="text-xl block font-bold mb-10">Setence Generator</h4>
+      <h4 className="text-xl block font-bold mb-4">Setence Generator</h4>
+      <p>Please enter a large amount of text containing at least 1000 words.</p>
       <form onSubmit={handleSubmit} className="py-4 grid grid-cols-1 gap-6">
         <textarea
           value={content}
@@ -43,19 +46,36 @@ export default function App() {
             border-secondary-300
             text-secondary-300 rounded-lg font-medium hover:text-primary-600 hover:border-primary-600 cursor-pointer"
             >
-              Submit
+              Generate
             </button>
+            {generatedContent ? (
+              <button
+                type="submit"
+                onClick={() => setGeneratedContent("")}
+                className="inline-block px-6 py-3 border
+            border-secondary-300 text-secondary-300 rounded-lg font-medium hover:text-primary-600 hover:border-primary-600 cursor-pointer"
+              >
+                Clear
+              </button>
+            ) : (
+              <></>
+            )}
           </div>
         )}
       </form>
 
-      {loading ? (
-        <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center">
+        {loading ? (
           <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary-600"></div>
-        </div>
-      ) : (
-        <></>
-      )}
+        ) : (
+          <p>{generatedContent}</p>
+        )}
+      </div>
+
+      <div className="mt-12">
+        <h4 className="text-xl block font-bold mb-4">How it Works</h4>
+        <p>...</p>
+      </div>
     </div>
   );
 }
---
+++ src/services/generate.ts
@@ -0,0 +1,32 @@
+export class Generator {
+  content: string;
+  model: Record<string, Record<string, number>>;
+
+  // Constructor
+  constructor(content: string) {
+    this.content = content;
+  }
+
+  // Methods
+
+  initModel(): void {
+
+    for (let i = 0; i < this.content.length; i++) {
+
+        
+    }
+    this.model = model;
+  }
+
+  createModel(): string {
+    return this.model;
+  }
+
+  generate(): string {
+    this.initModel();
+    this.
+    return this.content;
+  }
+
+}
+
